14de40835ea877cea2752aa128c3ec9f8c71108e
Touch Web <animatewithk@gmail.com>
2024-11-29 13:45:21 +0530

    message

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+alert("This website is under development and will not work now when the development is done this message will be removed and and the website will totally working")
+
 function gotoweb(website) {
     if (website == "Social Blog"){
         window.location.replace("/socialblog/socialBlog.html");
