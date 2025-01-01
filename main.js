651c502a591f503b70be9f6c59b9e8855f9bc9d2
Touch Web <animatewithk@gmail.com>
2025-01-01 13:16:46 +0530

    happy new year

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("This website is under development and will not work now when the development is done this message will be removed and the website will totally working")
+
+function gotoweb(website) {
+    if (website == "Social Blog"){
+        window.location.replace("/socialblog/socialBlog.html");
+    }
+}
